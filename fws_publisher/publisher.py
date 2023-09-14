c718025dc4f4a1db2039dceb1ef9370aa3e5307f
nishalangovender <nishalan.govender@gmail.com>
2023-09-14 19:40:34 +0200

    Updated Kinematics

--- fws_publisher/publisher.py
+++ fws_publisher/publisher.py
@@ -15,7 +15,7 @@ class KinematicsCalculator(Node):
         # Initialize subscribers for Twist messages from teleop_twist_keyboard
         self.twist_sub = self.create_subscription(
             Twist,
-            '/cmd_vel',  # Update to match the teleop_twist_keyboard topic
+            '/cmd_vel',
             self.twist_callback,
             10)
 
@@ -29,63 +29,62 @@ class KinematicsCalculator(Node):
             '/wheel_velocity_controller/commands',
             10)
 
-        # Define r as calss attribute
-        self.r = 0.065
-
-        # Define the given value for L as a class attribute
-        self.L = 0.2
-
-        # Initialize delta_A and delta_B as class attributes
-        self.delta_A = 0.0
-        self.delta_B = 0.0
-
     def twist_callback(self, msg):
-        # Extract linear and angular velocity from Twist message
-        V_x = msg.linear.x
-        V_y = msg.linear.y
-        V = math.sqrt(V_x ** 2 + V_y ** 2)
+
+        # Parameters
+        R = 0.065
+        L = 0.2
+
+        # Twist Messages
+        x_dot = msg.linear.x
+        beta = msg.linear.y
         psi_dot = msg.angular.z
 
-        # Access the L, delta_A, and delta_B attributes from the class
-        L = self.L
-        delta_A = self.delta_A
-        delta_B = self.delta_B
+        if beta == 0:
+            # Zero Side-Slip
 
-        # Handle the case when linear velocity (V) is zero
-        if V == 0:
-            cos_beta = 1.0  # Set a default value for cos_beta
-            cos_delta_A = 1.0  # Set default value for cos_delta_A
-            cos_delta_B = 1.0  # Set default value for cos_delta_B
-            tan_delta_A = 0.0
-            tan_delta_B = 0.0
+            # Steering Angle
+            if x_dot != 0:
+                delta = math.atan(0.5*psi_dot*L/x_dot)
+            else:
+                delta = 0
+            delta_A = delta
+            delta_B = -delta
+
+            # Wheel Velocity
+            v = x_dot/math.cos(delta)
+
         else:
-            # Calculate cos(β), cos(δ_A), cos(δ_B), tan(δ_A), and tan(δ_B)
-            denominator = 1 + (2 * L * psi_dot / V) ** 2
-            cos_beta = 1 / math.sqrt(denominator)
-            cos_delta_A = math.cos(math.atan(2 * L * psi_dot - V * (math.tan(
-                delta_B) - math.tan(delta_A)) / V * (math.tan(delta_B) + math.tan(delta_A))))
-            cos_delta_B = math.cos(math.atan(2 * L * psi_dot + V * (math.tan(
-                delta_B) - math.tan(delta_A)) / V * (math.tan(delta_B) + math.tan(delta_A))))
-            tan_delta_A = math.tan(delta_A)
-            tan_delta_B = math.tan(delta_B)
+            # Parallel Steering
 
-        # Calculate steering angles (delta_A and delta_B)
-        self.delta_A = math.atan2(
-            2 * L * psi_dot - V * (tan_delta_B - tan_delta_A), V * (tan_delta_B + tan_delta_A))
-        self.delta_B = math.atan2(
-            2 * L * psi_dot + V * (tan_delta_B - tan_delta_A), V * (tan_delta_B + tan_delta_A))
+            # Steering Angle
+            delta_A = beta
+            delta_B = beta
 
-        # Calculate wheel velocities (v_A and v_B)
-        v_A = V * cos_beta / cos_delta_A
-        v_B = V * cos_beta / cos_delta_B
+            # Wheel Velocity
+            if psi_dot == 0:                        # Given x_dot only
+                v = x_dot/(2*math.cos(beta))
+            else:                                   # Given both x_dot and psi_dot -> use psi_dot
+                v = psi_dot*L/math.sin(beta)
 
+        v_A = v
+        v_B = v
+
+        # Calculate steering angles
+        # delta_A_l = 
+        # delta_A_r = 
+        # delta_B_l = 
+        # delta_B_r = 
+        
         # Calculate wheel angular velocities (ω_A and ω_B)
-        omega_A = v_A / self.r
-        omega_B = v_B / self.r
+        v_A_wheel = v_A/math.sqrt(2)
+        v_A_wheel = v_B/math.sqrt(2)
+        omega_A = v_A_wheel / R
+        omega_B = v_A_wheel / R
 
         # Publish the calculated values to the command topics
         steering_msg = Float64MultiArray(
-            data=[self.delta_A, self.delta_A, self.delta_B, self.delta_B])
+            data=[delta_A, delta_A, delta_B, delta_B])
         wheel_msg = Float64MultiArray(
             data=[omega_A, omega_A, omega_B, omega_B])
 
