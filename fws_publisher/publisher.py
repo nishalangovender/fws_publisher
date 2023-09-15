b18efcab6f64d9826fb90058cc433779154a758c
nishalangovender <nishalan.govender@gmail.com>
2023-09-15 15:39:26 +0200

    Improved Logic

--- fws_publisher/publisher.py
+++ fws_publisher/publisher.py
@@ -40,51 +40,44 @@ class KinematicsCalculator(Node):
         beta = msg.linear.y
         psi_dot = msg.angular.z
 
-        if beta == 0:
-            # Zero Side-Slip
+        # Initialize delta to zero
+        delta = 0
 
-            # Steering Angle
-            if x_dot != 0:
-                delta = math.atan(0.5*psi_dot*L/x_dot)
+        # Initialize v to a default value
+        v = 0
+
+        if x_dot != 0:
+            if beta == 0:
+                # Zero Side-Slip
+
+                # Steering Angle
+                delta = math.atan(0.5 * psi_dot * L / x_dot)
+
+                # Wheel Velocity
+                v = x_dot / math.cos(delta)
+
             else:
-                delta = 0
-            delta_A = delta
-            delta_B = -delta
+                # Parallel Steering
 
-            # Wheel Velocity
-            v = x_dot/math.cos(delta)
+                # Steering Angle
+                delta = beta
 
-        else:
-            # Parallel Steering
-
-            # Steering Angle
-            delta_A = beta
-            delta_B = beta
-
-            # Wheel Velocity
-            if psi_dot == 0:                        # Given x_dot only
-                v = x_dot/(2*math.cos(beta))
-            else:                                   # Given both x_dot and psi_dot -> use psi_dot
-                v = psi_dot*L/math.sin(beta)
+                # Wheel Velocity
+                if psi_dot == 0:  # Given x_dot only
+                    v = x_dot / (2 * math.cos(beta))
+                else:  # Given both x_dot and psi_dot -> use psi_dot
+                    v = psi_dot * L / math.sin(beta)
 
         v_A = v
         v_B = v
 
-        # Calculate steering angles
-        # delta_A_l = 
-        # delta_A_r = 
-        # delta_B_l = 
-        # delta_B_r = 
-        
         # Calculate wheel angular velocities (ω_A and ω_B)
-        v_A_wheel = v_A/math.sqrt(2)
-        v_A_wheel = v_B/math.sqrt(2)
-        omega_A = v_A_wheel / R
-        omega_B = v_A_wheel / R
+        omega_A = v_A / R
+        omega_B = v_B / R
 
         # Publish the calculated values to the command topics
         steering_msg = Float64MultiArray(
-            data=[delta_A, delta_A, delta_B, delta_B])
+            data=[delta, delta, -delta, -delta])
         wheel_msg = Float64MultiArray(
             data=[omega_A, omega_A, omega_B, omega_B])
 
